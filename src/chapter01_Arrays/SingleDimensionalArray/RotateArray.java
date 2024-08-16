5076f071688c731eca94f6a95eeb92acc4292df2
itsme-swe <me.harshmehra07@gmail.com>
2024-08-16 20:32:51 +0530

    update

--- src/chapter01_Arrays/SingleDimensionalArray/RotateArray.java
+++ src/chapter01_Arrays/SingleDimensionalArray/RotateArray.java
@@ -8,6 +8,35 @@ public class RotateArray {
 
   public static void main(String[] args) {
 
+    int[] arr = { 10, 20, 30, 40, 70, 80, 90 };
+
+    int k = 3;
+
+    RotateArray rotateArray = new RotateArray();
+    rotateArray.rotate(arr, k);
+
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i] + " ");
+    }
+
+  }
+
+  public void rotate(int[] a, int k) {
+    int n = a.length;
+    k = k % n;
+    reverse(a, 0, n - k - 1);
+    reverse(a, n - k, n - 1);
+    reverse(a, 0, n - 1);
+  }
+
+  public void reverse(int[] nums, int i, int j) {
+    while (i < j) {
+      int temp = nums[i];
+      nums[i] = nums[j];
+      nums[j] = temp;
+      i++;
+      j--;
+    }
   }
 
 }
