4c926d25762d460db222fb5e983336188205c069
itsme-swe <me.harshmehra07@gmail.com>
2024-08-26 07:29:10 +0530

    update

---
+++ src/chapter01_Arrays/MultiDimenArrays/largestEleIn2DArray.java
@@ -0,0 +1,26 @@
+package chapter01_Arrays.MultiDimenArrays;
+
+/*
+ * Find the largest element of a given 2D array of Integers.
+ */
+public class largestEleIn2DArray {
+
+  public static void main(String[] args) {
+
+    int[][] arr = { { 1, 5, 7 }, { 2, -3, 6 }, { 2, 4, 5 } };
+
+    int max = Integer.MIN_VALUE;
+
+    int row = arr.length;
+    int col = arr[0].length;
+
+    // Yaha pae 'i' chalta hai row kae liye aur 'j' chalta hai columns kae liye
+    for (int i = 0; i < row; i++) {
+      for (int j = 0; j < col; j++) {
+        max = Math.max(max, arr[i][j]);
+      }
+    }
+    System.out.println("The maximum element in 2D Array is " + max);
+  }
+
+}
