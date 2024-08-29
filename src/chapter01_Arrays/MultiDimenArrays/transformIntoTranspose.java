aa740b6e09bd44bebd5868fbda1b5b762303ad2f
itsme-swe <me.harshmehra07@gmail.com>
2024-08-29 07:22:31 +0530

    update

---
+++ src/chapter01_Arrays/MultiDimenArrays/transformIntoTranspose.java
@@ -0,0 +1,43 @@
+package chapter01_Arrays.MultiDimenArrays;
+
+/*
+ * In this question we need to transpose the matrix without creating an new matrix.
+ */
+public class transformIntoTranspose {
+
+  public static void main(String[] args) {
+
+    int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+    printArray(matrix);
+
+  }
+
+  // »» Function to print array
+  public static void printArray(int[][] arr) {
+
+    transformToTranspose(arr);
+
+    for (int[] row : arr) {
+      for (int ele : row) {
+        System.out.print(ele + " ");
+      }
+      System.out.println();
+    }
+  }
+
+  // »» Function to transposeArray
+  public static void transformToTranspose(int[][] matrix) {
+
+    int m = matrix.length;
+
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j <= i; j++) {
+        int temp = matrix[i][j];
+        matrix[i][j] = matrix[j][i];
+        matrix[j][i] = temp;
+      }
+    }
+  }
+
+}
